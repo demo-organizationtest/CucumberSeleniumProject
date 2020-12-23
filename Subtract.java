d6a0ac68f5a854bccc2ecef74706f460afeeaf07
SharvarW <49506491+SharvarW@users.noreply.github.com>
2020-12-23 14:38:30 +0530

    updated Subtract.java
    Added changes

--- Subtract.java
+++ Subtract.java
@@ -1,6 +1,11 @@
 class subtract
 {
-    int n1=1,n2=3;
+    int n1,n2;
+    scanner sc=new Scanner(System.in);
+    System.out.println("enter no. 1");
+    n1=sc.nextInt();
+    System.out.println("enter no. 2");
+    n2=sc.nextInt();
     int result=n2-n1;
     system.out.println("Result is : "+result);
  }
