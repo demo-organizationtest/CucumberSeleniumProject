d24b573711e7b03d14a218d583c5e5e4d8cfa27a
SharvarW <49506491+SharvarW@users.noreply.github.com>
2020-12-23 15:50:15 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,6 +1,8 @@
 class add
 {
-    int n1=1,n2=2;
+    int n1,n2;
+    System.out.println("Enter no.1"+n1);
+    System.out.println("Enter no.2"+n2);
     int result;
     result=n1+n2;
     System.out.println("the result is : "+result);
