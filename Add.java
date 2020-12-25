5a5c635a670e6635d0c21cbf156baed9108178bb
SharvarW <49506491+SharvarW@users.noreply.github.com>
2020-12-25 15:16:04 +0530

    updated add.java second time

--- Add.java
+++ Add.java
@@ -4,7 +4,7 @@ class add
     System.out.println("Enter no.1"+no1);
     System.out.println("Enter no.2"+no2);
     int result;
-    result=n1+n2;
+    result=no1+no2;
     System.out.println("the result is : "+result);
 } 
   
